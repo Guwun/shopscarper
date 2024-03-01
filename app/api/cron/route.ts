d7c4c649406fe0ff80dc23ac19670207bb44f34e
Guwun <fatemehsalimi0497@gmail.com>
2024-03-01 05:55:35 -0700

    first commit

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -2,10 +2,12 @@ import Product from "@/lib/models/product.model";
 import { connectToDB } from "@/lib/mongoose";
 import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
 import { scrapeAmazonProduct } from "@/lib/scraper";
-import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } from "@/lib/utils";
-import { get } from "http";
+import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } from "@/lib/utils"
 import { NextResponse } from "next/server";
 
+export const maxDuration = 300; // 5 minutes
+export const dynamic = 'force-dynamic'
+export const revalidate = 0;
 
 export async function GET() {
     try {
